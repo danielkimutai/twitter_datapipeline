f337b1b782a87b1c88be05f56e9a6ffcb3c82bfe
Daniel Kimutaii <danielkimutaikorir@gmail.com>
2024-09-17 20:29:23 +0300

    s

--- twitter_etl.py
+++ twitter_etl.py
@@ -1,29 +1,21 @@
-import tweepy
-import pandas as pd
-import json
-from datetime import datetime
-import fs  # PyFilesystem
-from google.cloud import storage 
+from twikit  import Client ,TooManyRequests 
+import time 
+from datetime import datetime 
+import csv 
+from configparser import ConfigParser 
+from random import randint   
 
-# twitter api keys
-access_key = "FSiSuquUPIqW2iIj0zdZjYuzu"
-access_secret="KnvGiOImdnktFEvtOqbJMTPqX5b48VUVHJJRVyBr1trleu9hJu" 
-consumer_key ="1208858853535825923-bMoNZXwsgUINczMM8G7A3Jf6CpMr0b"
-consumer_secret="kMxvQrazkDIXTfEMdxLO0tm4eyijuAazNGx6K9jaWMh22"
+minimum_tweets = 10
+query = 'chatgpt'
 
-#twitter authentication 
-auth = tweepy.OAuthHandler(access_key,access_secret)
-auth.set_access_token(consumer_key, consumer_secret)
+#* login credentials 
+config = ConfigParser()
+config.read('config.ini')
+username = config['X']['username']
+email = config['X']['email']
+password = config['X']['password']
 
-#creating an API object 
-api = tweepy.API(auth)
-
-#scraping a users twitter 
-tweets = api.user_timeline(screen_name="kimvtaii",
-                            # 200 is the maximum allowed count 
-                            count=200,
-                            include_rts=False,
-                            # Necessary to keep full_text 
-                            tweet_mode="extended"
-                            )
-print(tweets)
+#authenticate to X.com
+client = Client(language='en-US')
+client.login(auth_info_1=username,auth_info_2=email,password=password)
+client.save_cookies('cookies.json')
